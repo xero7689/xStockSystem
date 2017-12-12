d76eafd27345993a24a413d946a587a5f80f2c78
peter.lee <peter.lee@lionic.com>
2017-12-12 14:38:52 +0800

    Testing test.py

--- analysis/test.py
+++ analysis/test.py
@@ -1,9 +1,16 @@
 import pymysql
 import pandas as pd
 
+db = pymysql.connect('localhost', 'xero', 'uscrfycn', 'taiwan_stock')
+def get_stock_info(sid):
+    sql = """
+        select date, price, yield from daily where stock_id = %d;
+    """ % sid
+    df = pd.read_sql(sql, con=db)
+    return df
+
 if __name__ == '__main__':
-    db = pymysql.connect('localhost', 'xero', 'uscrfycn', 'taiwan_stock')
-    df = pd.read_sql('select date, price, yield from daily where stock_id = 1101;', con=db)
+    df = get_stock_info(1101)
     print(df)
     df['date'] = pd.to_datetime(df['date'])
     b = df['date'].dt
