2ca5b1f6eb27f9551b0a5f9697d7be0357881a88
peter.lee <peter.lee@lionic.com>
2017-12-13 15:54:44 +0800

    Change test.py to count differnet combination's correlation.

--- analysis/test.py
+++ analysis/test.py
@@ -1,18 +1,24 @@
-import pymysql
-import pandas as pd
+import sys, os
+import time
+from select_stock import make_stock_combination 
+from analyze_stock import count_corr
+from lib.mysql import get_stock_info, get_zh_name
+#import pandas as pd
 
-db = pymysql.connect('localhost', 'xero', 'uscrfycn', 'taiwan_stock')
-def get_stock_info(sid):
-    sql = """
-        select date, price, yield from daily where stock_id = %d;
-    """ % sid
-    df = pd.read_sql(sql, con=db)
-    return df
 
 if __name__ == '__main__':
-    df = get_stock_info(1101)
-    print(df)
-    df['date'] = pd.to_datetime(df['date'])
-    b = df['date'].dt
-    print(df.groupby([b.year])['price'].mean())
-    print(df.groupby([b.year])['price'].std())
+    stock_comb = make_stock_combination()
+    for sid1, sid2 in stock_comb:
+        info1 = get_stock_info(int(sid1))
+        info2 = get_stock_info(int(sid2))
+        corr = count_corr(info1, info2)
+        if corr > 0.7:
+            zh1 = get_zh_name(sid1)[0][0]
+            zh2 = get_zh_name(sid2)[0][0]
+            print('{}\t{}\tCorr: {}'.format(zh1, zh2, corr))
+        elif corr < 0:
+            zh1 = get_zh_name(sid1)[0][0]
+            zh2 = get_zh_name(sid2)[0][0]
+            print('{}\t{}\tCorr: {}'.format(zh1, zh2, corr))
+        time.sleep(1)
+
