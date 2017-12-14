5558ab13588ff1e84cbb1523264f18d494acfe06
xero7689 <volleyp7689@gmail.com>
2017-12-14 13:12:38 +0800

    Split aligned date section to single function.

--- analysis/analyze_stock.py
+++ analysis/analyze_stock.py
@@ -1,5 +1,28 @@
 import sys, os
 
+def align_price_by_date(df1, df2, how='inner'):
+    """
+    Your DataFrame must be combined with `date` and `price` field
+
+    Args:
+        df1 (DataFrame): First DataFrame going to be aligned
+        df2 (DataFrame): Second DataFrame going to be aligned
+        how (str)      : Method to join DataFrame. `inner` or `outer`.
+    Returns:
+        DataFrame: Pandas DataFrame after aligned by date
+    """
+    try:
+        dfp1 = df1[['date', 'price']]
+        dfp2 = df2[['date', 'price']]
+    except Exception as e:
+        raise e
+
+    # Align by merge
+    # inner - both has data
+    # outer - will have NaN
+    df_m = dfp1.merge(dfp2, how=how, left_on='date', right_on='date')
+    return df_m
+
 def count_corr(info1, info2):
     df1 = info1[['date', 'price']]
     df2 = info2[['date', 'price']]
