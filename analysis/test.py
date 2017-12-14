06b3fffe05f3491aaf3430896073d0ae305666b2
xero7689 <volleyp7689@gmail.com>
2017-12-14 13:13:12 +0800

    Write result to file

--- analysis/test.py
+++ analysis/test.py
@@ -8,17 +8,22 @@ from lib.mysql import get_stock_info, get_zh_name
 
 if __name__ == '__main__':
     stock_comb = make_stock_combination()
+    pre_id = None
     for sid1, sid2 in stock_comb:
-        info1 = get_stock_info(int(sid1))
+        if pre_id != sid1:
+            info1 = get_stock_info(int(sid1))
         info2 = get_stock_info(int(sid2))
         corr = count_corr(info1, info2)
         if corr > 0.7:
             zh1 = get_zh_name(sid1)[0][0]
             zh2 = get_zh_name(sid2)[0][0]
+            with open('positive.lst') as f:
+                f.write('{}\t{}\t{}'.format())
             print('{}\t{}\tCorr: {}'.format(zh1, zh2, corr))
         elif corr < 0:
             zh1 = get_zh_name(sid1)[0][0]
             zh2 = get_zh_name(sid2)[0][0]
             print('{}\t{}\tCorr: {}'.format(zh1, zh2, corr))
-        time.sleep(1)
+        pre_id = sid1
+        time.sleep(0.5)
 
