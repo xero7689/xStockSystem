5d3a1c1181b0c429a57152b5230bf218bc1f5e84
xero7689 <volleyp7689@gmail.com>
2018-02-02 13:32:03 +0800

    Rewrite crawler

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,89 +1,40 @@
-import sys
+import sys, os
 import pymysql
-import requests
-import json
-import datetime
-import time
-import logging
+
+from utils import year_generator
+from daily import *
+from proxy_pool import ProxyPool
 from settings import *
 
-def year_generator(start_year=2010, end_year=2017):
-    dates = []
-    for y in range(start_year, end_year+1):
-        year = str(y)
-        for m in range(1, 12+1):
-            month = str(m)
-            if m < 10:
-                month = '0{}'.format(month)
-            date = '{}{}01'.format(year, month)
-            dates.append(date)
-    return dates
+pp = ProxyPool()
 
-def crawl_day_avg(stock_no, date):
-    if int(date) > 20170923:
-        return -1
-    request_url = url.format(date, stock_no)
-    response = requests.get(request_url)
-    if not response.content:
-        print("[-] {}".format(date))
-        return -1
-    data = json.loads(response.content)
-    price_data = data.get('data', None)
-    if not price_data:
-        print('[-] {}'.format(date))
-        return -1
-    insert_len = len(price_data)
-    tmp_sql = []
-    for d in price_data:
-        # d[0]: date, d[1]: price
-        try:
-            price_date = d[0]
-            price = d[1]
-            price_date = price_date.split('/')
-            price_year = price_date[0].strip()
-            price_year = str(int(price_year) + 1911)
-            price_month = price_date[1]
-            price_day = price_date[2]
-            sql_date = "{}-{}-{}".format(price_year, price_month, price_day)
-            tmp_sql.append("('{}', '{}')".format(sql_date, d[1]))
-        except:
-            print('[-] {}'.format(date))
-            continue
-        print("[+] {}".format(date))
-
-    value_sql = ",".join(tmp_sql)
-    sql = batch_insert_sql.format(stock_no, value_sql)
-    response = cursor.execute(sql)
-    db.commit()
+def get_cate_stock(cate_name):
+    sql = """
+        SELECT stock_id from stock_list
+        WHERE stock_cate = %s
+    """
+    sidlist = []
+    con = pymysql.connect(HOST, USER, PASSWORD, 'taiwan_stock', charset='utf8')
+    cursor = con.cursor()
+    cursor.execute(sql, (cate_name,))
+    response = cursor.fetchall()
+    cursor.close()
+    con.close()
+    for sid in response:
+        sidlist.append(sid[0])
+    return sidlist
 
 if __name__ == '__main__':
-    db = pymysql.connect(HOST, USER, PASSWORD, 'taiwan_stock')
-    cursor = db.cursor()
-    
-    create_table_sql = """
-    CREATE TABLE IF NOT EXISTS `{}_day_avg` (
-      `date` date DEFAULT NULL UNIQUE,
-      `price` double DEFAULT NULL
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
-    """
+    stock_list = get_cate_stock('電子零組件')
+    print(stock_list)
+    for sid in stock_list:
+        try:
+            sid = int(sid)
+        except:
+            continue
+        for date in year_generator(start_year=2012):
+            print(crawl_daily_data(sid, date, False))
+            print(crawl_daily_bwibbw(sid, date, False))
+            break
+        break
 
-    batch_insert_sql = """
-    INSERT IGNORE INTO {}_day_avg
-    (date, price)
-    VALUES {}
-    """
-
-    #stock_no = sys.argv[1]
-    stock_no = [1101, 1102, 1103, 1104, 1108, 1109, 1110]
-    url = 'http://www.tse.com.tw/exchangeReport/STOCK_DAY_AVG?response=json&date={}&stockNo={}'
-
-    dates = year_generator()
-
-    db.commit()
-    for sno in stock_no:
-        cursor.execute(create_table_sql.format(sno))
-        for date in dates:
-            crawl_day_avg(sno, date)
-            time.sleep(5)
-        
-        
