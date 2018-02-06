5fcb34c7c3ff5035dbb798f31f36831573babb14
xero <volleyp7689@gmail.com>
2018-02-06 21:58:19 +0800

    Add category crawler

---
+++ crawler/cate_crawler.py
@@ -0,0 +1,45 @@
+import sys, os
+from pprint import pprint
+import pymysql
+
+from utils import year_generator
+from daily import *
+from proxy_pool import ProxyPool
+from settings import *
+
+pp = ProxyPool()
+
+def get_cate_stock(cate_name):
+    sql = """
+        SELECT stock_id from stock_list
+        WHERE stock_cate = %s
+    """
+    sidlist = []
+    con = pymysql.connect(HOST, USER, PASSWORD, 'taiwan_stock', charset='utf8')
+    cursor = con.cursor()
+    cursor.execute(sql, (cate_name,))
+    response = cursor.fetchall()
+    cursor.close()
+    con.close()
+    for sid in response:
+        sidlist.append(sid[0])
+    return sidlist
+
+if __name__ == '__main__':
+    cate = sys.argv[1]
+    stock_list = get_cate_stock(cate)
+    print(stock_list)
+    use_proxy = int(sys.argv[2])
+    for sid in stock_list:
+        try:
+            sid = int(sid)
+        except:
+            continue
+        for date in year_generator(start_year=2012):
+            dd = crawl_daily_data(sid, date, use_proxy)
+            if dd:
+                insert_daily_data(sid, dd)
+            bd = crawl_daily_bwibbw(sid, date, use_proxy)
+            if bd:
+                insert_bwibbw_data(sid, bd)
+            time.sleep(1)
