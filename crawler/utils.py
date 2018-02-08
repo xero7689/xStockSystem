45fb4b22ed0230474b0a41be0e1fce160826f12b
xero7689 <volleyp7689@gmail.com>
2018-02-08 14:37:50 +0800

    Add until today generator

--- crawler/utils.py
+++ crawler/utils.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import datetime
 
 def year_generator(start_year=2010, end_year=2017):
     dates = []
@@ -13,6 +14,19 @@ def year_generator(start_year=2010, end_year=2017):
             dates.append(date)
     return dates
 
+def until_today_generator(start_year=2010):
+    dates = []
+    for y in range(start_year, datetime.datetime.now().year+1):
+        year = str(y)
+        for m in range(1, 12+1):
+            month = str(m)
+            if m < 10:
+                month = '0{}'.format(month)
+            date = '{}{}01'.format(year, month)
+            if date < datetime.datetime.now().strftime('%Y%m%d'):
+                dates.append(date)
+    return dates
+
 if __name__ == '__main__':
-    for y in year_generator():
+    for y in until_today_generator(start_year=2018):
         print(y)
