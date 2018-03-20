f20dafee4c75eaad1195dcd360f54a5eb4323298
xero7689 <volleyp7689@gmail.com>
2018-03-20 11:00:32 +0800

    Rewrite update stock script.

--- crawler/update.py
+++ crawler/update.py
@@ -18,30 +18,15 @@ def find_update_date(sid):
     return date_list
 
 if __name__ == '__main__':
-    sid = int(sys.argv[1])
-    date_list = find_update_date(sid)
-    cur_month = None
-    for date in until_today_generator(2017):
-        """
-        # Parse date
-        year = date.year
-        month = date.month
-        if month == cur_month:
-            continue
-        else:
-            cur_month = month
-        day = date.day
-        if month < 10:
-            month = "0{}".format(month)
-        if day < 10:
-            day = "0{}".format(day)
-        _date = "{}{}{}".format(year, month, day)
-        """
-
-        # Crawl
-        #crawl_daily(_date, sid)
-        dd = crawl_daily_data(sid, date, False)
-        bd = crawl_daily_bwibbw(sid, date, False)
-        insert_daily_data(sid, dd)
-        insert_bwibbw_data(sid, bd)
-        time.sleep(random.randint(5, 15))
+    db = StockMySQL('127.0.0.1', USER, PASSWORD, 'taiwan_stock')
+    dsid = db.select_distinct_sid_from_daily()
+    #sid = int(sys.argv[1])
+    for sid in dsid:
+        sid = sid[0]
+        date_list = find_update_date(sid)
+        for date in until_today_generator(2018):
+            dd = crawl_daily_data(sid, date, False)
+            bd = crawl_daily_bwibbw(sid, date, False)
+            insert_daily_data(sid, dd)
+            insert_bwibbw_data(sid, bd)
+            time.sleep(random.randint(5, 15))
