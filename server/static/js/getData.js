0e626bb93217022c4b1c3455bc86656305a57c42
xero7689 <volleyp7689@gmail.com>
2019-03-01 16:36:02 +0800

    Update section of fetch API

--- server/static/js/getData.js
+++ server/static/js/getData.js
@@ -11,6 +11,18 @@ var getData = (function () {
         return jsonResponse;
     };
 
+
+    /* Get price from self API ?!
+     */
+    var get_rank_list = function() {
+        var req = new XMLHttpRequest();
+        req.open('GET', 'http://127.0.0.1:5000/mysql', false);
+        req.send( null );
+        response = req.responseText;
+        var jsonResponse = JSON.parse(response);
+        return jsonResponse;
+    };
+
     return {
         get_rank_list: get_rank_list
     }
