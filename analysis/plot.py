cc61cd9978217ce61e13a8dfe54a96d315009908
xero <volleyp7689@gmail.com>
2019-03-24 10:51:26 +0800

    Split color-scheme section

--- analysis/plot.py
+++ analysis/plot.py
@@ -12,6 +12,10 @@ import pandas as pd
 from lib.mysql import get_all_stock_info, get_zh_name
 from settings import chinese_font_path
 
+# Color Scheme
+FACE_COLOR = '#F1EFE2'
+EDGE_COLOR = '#E7E4D3'
+GRID_COLOR = '#C0C0C0'
 
 #fig, ax = plt.subplots(figsize=(15, 7))
 ax = plt.subplot2grid((7,4), (1,0), rowspan=4, colspan=4, axisbg='#07000d')
@@ -33,10 +37,10 @@ ddf = df[mask]
 
 # Set xaxis
 ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%y %b %d'))
 
 # Plot
-#ax.plot(ddf['date'], ddf['price'])
+ax.plot(ddf['date'], ddf['price'])
 ax.plot(ddf['date'], ddf['price'].rolling(window=7).mean(), color='#FFD700', linewidth=0.7, ls='--')
 ax.plot(ddf['date'], ddf['price'].rolling(window=14).mean(), color='#FFA500', linewidth=0.7, ls='--')
 ax.plot(ddf['date'], ddf['price'].rolling(window=28).mean(), color='#FF8C00', linewidth=0.7, ls='--')
@@ -50,41 +54,44 @@ candlestick_ohlc(ax, ddfreshape.values, width=0.6, colorup='#DC143C', colordown=
 
 # Plot vol
 axv = ax.twinx()
-axv.fill_between(ddfreshape['date'].values, 0, ddfreshape['lot'].values, facecolor='#87CEFA', alpha=.4)
+axv.fill_between(ddfreshape['date'].values, 0, ddfreshape['lot'].values, facecolor=FACE_COLOR, alpha=.4)
 axv.set_ylim(0, max(3*ddfreshape['lot'].values))
 axv.axes.yaxis.set_ticklabels([])
 axv.grid(False)
 
 # AX-PE
-axpe = plt.subplot2grid((7,4), (0,0), sharex=ax, rowspan=1, colspan=4, axisbg='#07000d')
-axpe.plot(ddf['date'], ddf['yield'], color='#9932CC')
+axpe = plt.subplot2grid((7,4), (0,0), sharex=ax, rowspan=1, colspan=4, axisbg=FACE_COLOR)
+axpe.plot(ddf['date'], ddf['pe'], color='#206BA4')
 mean_pe = ddf['pe'].mean()
 axpe.axhline(y=mean_pe, linewidth='0.6', color='#D8BFD8', ls='--')
+#axpe.set_ylim(min(ddf['pe']), max(ddf['pe']))
 axpe.legend(['PE'], prop=font)
 axpe.set_ylabel('PE')
 
 # AX-BIAS
-axbias = plt.subplot2grid((7,4), (5,0), sharex=ax, rowspan=1, colspan=4, axisbg='#07000d')
+axbias = plt.subplot2grid((7,4), (5,0), sharex=ax, rowspan=1, colspan=4, axisbg='#F1EFE2')
 ma_7 = ddf['price'].rolling(window=7).mean()
 sp = len(ddf['price']) - len(ma_7)
 rsi = (ddf['price'][sp:] - ma_7[sp:])/(ma_7[sp:])
-axbias.plot(ddf['date'], rsi, color='#FFDEAD')
+axbias.plot(ddf['date'], rsi, color='#206BA4')
 axbias.axhline(y=0.0, linewidth='0.6', color='red', ls='--')
 axbias.legend(['乖離率'], prop=font)
 
 # Some plt settings
-ax.patch.set_facecolor('#0C090A')
-ax.patch.set_edgecolor('#0C090A')
-ax.grid(color='#2F4F4F')
-axpe.grid(color='#2B547E')
-axbias.grid(color='#2B547E')
-ax.legend(['7日均線', '14日均線', '28日均線'], prop=font)
+
+ax.patch.set_facecolor(FACE_COLOR)
+ax.patch.set_edgecolor(EDGE_COLOR)
+ax.grid(color=GRID_COLOR)
+axpe.grid(color=GRID_COLOR)
+axbias.grid(color=GRID_COLOR)
+ax.legend(['股價', '7日均線', '14日均線', '28日均線'], prop=font)
 ax.set_ylabel('Price')
 axv.set_ylabel('Vol')
 start_date = (ddf['date'][ddf.index[0]]).strftime('%Y-%m-%d')
 end_date = (ddf['date'][ddf.index[-1]]).strftime('%Y-%m-%d')
 axpe.set_title('[{}] {} {}~{}'.format(sid, zh_name, start_date, end_date), fontproperties=font)
-plt.setp(ax.get_xticklabels(), visible=False)
-plt.setp(axpe.get_xticklabels(), visible=False)
+plt.setp(ax.get_xticklabels(), visible=False, rotation=65)
+plt.setp(axpe.get_xticklabels(), visible=False, rotation=65)
+plt.setp(axbias.get_xticklabels(), visible=True, rotation=65)
 plt.savefig("test.png",bbox_inches='tight')
 plt.show()
